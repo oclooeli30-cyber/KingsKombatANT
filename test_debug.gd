670f62a70a77151c930ee45a674daab4cdd302f7
oclooeli30-cyber <>
2026-08-06 13:20:01 +0800

    Auto-backup: 2026-08-06 13:20:01

--- test_debug.gd
+++ test_debug.gd
@@ -9,25 +9,44 @@ func _init() -> void:
     var player = level.get_node("Player")
     var fox = level.get_node("Fox")
 
-    print("--- settle ---")
     for i in range(80):
         await physics_frame
-    print("fox on_floor=", fox.is_on_floor())
 
+    fox.direction = 0
+    fox.global_position = player.global_position + Vector2(16, 0)
+    for i in range(3):
+        await physics_frame
+
+    print("1. overlap adjacent: ",
+          fox.get_node("Area2D").get_overlapping_areas().has(player.get_node("EnemyDetector")))
+
+    print("2. attack hits fox")
+    print("   fox LIFE before=", fox.LIFE)
+    player.attacking = true
+    player.get_node("AnimatedSprite2D").play("attack")
+    for i in range(60):
+        await physics_frame
+    print("   fox LIFE after=", fox.LIFE, " player attacking reset=", player.attacking == false)
+
+    print("3. second hit kills fox (ignore knockback by re-parking)")
     fox.LIFE = 1
-    print("calling _take_damage, anim=", fox.FOX.animation)
-    fox._take_damage()
-    print("after call, hurtfreeze=", fox.hurtfreeze, " anim=", fox.FOX.animation)
-    fox.FOX.animation_finished.connect(func(): print("FOX animation_finished fired, anim=", fox.FOX.animation))
+    fox.hurtfreeze = false
+    fox.global_position = player.global_position + Vector2(16, 0)
+    player.attacking = true
+    player.get_node("AnimatedSprite2D").play("attack")
+    for i in range(120):
+        await physics_frame
+    print("   fox alive after lethal: ", is_instance_valid(fox))
 
-    for i in range(90):
-        await process_frame
-        if not is_instance_valid(fox):
-            print("fox freed at frame ", i)
-            break
-    print("fox alive at end: ", is_instance_valid(fox))
-    if is_instance_valid(fox):
-        print("fox anim=", fox.FOX.animation, " hurtfreeze=", fox.hurtfreeze)
+    print("4. player touch damage")
+    print("   player LIFE before=", player.LIFE)
+    player.invis = 0
+    player.hurtfreeze = false
+    var hp = player.global_position
+    player.global_position = fox.global_position if is_instance_valid(fox) else hp + Vector2(20, 0)
+    for i in range(120):
+        await physics_frame
+    print("   player LIFE after=", player.LIFE)
 
     print("TEST DONE")
     quit()
