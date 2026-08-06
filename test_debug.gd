218158fc4e6f1d0ac7ce07cb11d05098eaead520
oclooeli30-cyber <>
2026-08-06 13:12:15 +0800

    Auto-backup: 2026-08-06 13:12:15

--- test_debug.gd
+++ test_debug.gd
@@ -9,37 +9,19 @@ func _init() -> void:
     var player = level.get_node("Player")
     var fox = level.get_node("Fox")
 
-    print("--- let everyone land on the ground ---")
-    for i in range(80):
-        await physics_frame
-    print("player pos=", player.global_position, " on_floor=", player.is_on_floor())
-    print("fox pos=", fox.global_position, " on_floor=", fox.is_on_floor())
-
-    print("--- park fox adjacent to player on the ground ---")
-    fox.direction = 0
-    fox.global_position = player.global_position + Vector2(16, 0)
-    for i in range(3):
-        await physics_frame
-    var hitbox = fox.get_node("Area2D")
-    print("overlap adjacent: ",
-          hitbox.get_overlapping_areas().has(player.get_node("EnemyDetector")))
-
-    print("--- attack ---")
-    print("fox LIFE before=", fox.LIFE)
-    player.attacking = true
-    player.get_node("AnimatedSprite2D").play("attack")
-    for i in range(25):
-        await physics_frame
-    print("fox LIFE after=", fox.LIFE)
-
-    print("--- lethal hit ---")
+    print("--- lethal hit directly ---")
     fox.LIFE = 1
-    fox.hurtfreeze = false
+    fox._take_damage()
+    for i in range(60):
+        await process_frame
+    print("fox alive after lethal: ", is_instance_valid(fox))
+
+    print("--- attack finishes via signal (no stuck state) ---")
     player.attacking = true
     player.get_node("AnimatedSprite2D").play("attack")
-    for i in range(45):
-        await physics_frame
-    print("fox alive after lethal: ", is_instance_valid(fox))
+    for i in range(30):
+        await process_frame
+    print("player attacking=", player.attacking)
 
     print("TEST DONE")
     quit()
