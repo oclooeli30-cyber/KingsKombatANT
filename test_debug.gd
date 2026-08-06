7feb90e349efb5563eab5dd442a8f43d872eb8ca
oclooeli30-cyber <>
2026-08-06 13:09:49 +0800

    Auto-backup: 2026-08-06 13:09:49

--- test_debug.gd
+++ test_debug.gd
@@ -8,36 +8,33 @@ func _init() -> void:
 
     var player = level.get_node("Player")
     var fox = level.get_node("Fox")
+    var hitbox = fox.get_node("Area2D")
 
-    print("--- player methods now present ---")
-    print("damage=", player.has_method("damage"),
-          " _on_area_2d_body_entered=", player.has_method("_on_area_2d_body_entered"),
-          " _on_animated_sprite_2d_animation_finished=", player.has_method("_on_animated_sprite_2d_animation_finished"))
-
-    print("--- overlap fox EnemyDetector with player, then attack ---")
-    fox.global_position = player.global_position
-    for i in range(6):
+    print("--- hold fox overlapping player's EnemyDetector ---")
+    for i in range(8):
+        fox.global_position = player.global_position
         await physics_frame
 
-    var overlapped = fox.get_node("Area2D").get_overlapping_areas()
-    print("fox area overlaps player EnemyDetector: ", overlapped.has(player.get_node("EnemyDetector")))
+    print("fox area overlaps player EnemyDetector: ",
+          hitbox.get_overlapping_areas().has(player.get_node("EnemyDetector")))
 
-    print("fox LIFE before attack=", fox.LIFE)
+    print("--- player attacks while overlapping ---")
+    print("fox LIFE before=", fox.LIFE, " attacking=", player.attacking)
+    player.invis = 0
     player.attacking = true
     player.get_node("AnimatedSprite2D").play("attack")
-    for i in range(15):
+    for i in range(20):
+        fox.global_position = player.global_position
         await physics_frame
-    print("fox LIFE after attack=", fox.LIFE)
+    print("fox LIFE after=", fox.LIFE, " player attacking=", player.attacking)
 
-    print("--- player takes damage ---")
+    print("--- player damage (clear invis first) ---")
+    player.invis = 0
     var before = player.LIFE
     player.damage()
     for i in range(10):
         await process_frame
     print("player LIFE ", before, " -> ", player.LIFE)
-
-    print("--- attack completes via signal (attacking resets) ---")
-    print("player attacking=", player.attacking)
 
     print("TEST DONE")
     quit()
